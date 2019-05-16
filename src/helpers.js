3ee8c3cd826659f697b43252c9e605f8f87c634b
jonsamp <jon@codecademy.com>
2019-05-15 21:26:33 -0400

    fix date filtering

--- src/helpers.js
+++ src/helpers.js
@@ -8,7 +8,7 @@ export const relativeDates = () => ({
 
 export const filterInvalidDates = dates =>
   dates.filter(date =>
-    !isValid(date)
+    !isValid(new Date(date))
       ? console.error(
           `The date '${date}' is not in a valid date format and date-streaks is ignoring it. Browsers do not consistently support this and this package's results may fail. Verify the array of dates you're passing to date-streaks are all valid date strings. http://momentjs.com/docs/#/parsing/string/`
         )
